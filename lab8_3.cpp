c97fc6f1b8b0e08e625d4d008044db407b87d7fd
svre-fm <firmoioi96@gmail.com>
2024-12-06 15:42:36 +0700

    complete lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    string character;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if(height < 100) character = "Chopper";
+        else if(height < 180) character = "Usopp";
+            else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+
+                if(bounty > 1.1*pow(10,9)) character = "Zoro";
+                else character = "Sanji";
+            }
+    }else{
+        if(age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if(bounty > 500*pow(10,6)) character = "Jinbe";
+            else character = "Franky";
+
+        }else character = "Brook";
+    }
+    
+    cout << "Your character = " << character ;
+
+    
+}
+
